8d53e61a98b96e8ede4d33f66afcbc860e57eb14
naorlivne <naorlivne@gmail.com>
2021-09-02 16:04:34 +0300

    adding hardware and basic readme and test screen and keypad code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # arduino_tnt_bomb_defuse_game
 Somebody set up the bomb
+
+## Software
+
+### Install liberies:
+Keypad (version 3.1.1)
+Adafruit SSD1306 (version 2.4.6)
+Adafruit GFX Library (version 1.10.10)
+Adafruit BusIO (version 1.9.1)
+
+## Hardware
+
+
+## Electronics
---
+++ arduino_tnt_defuse_game/arduino_tnt_defuse_game.ino
@@ -0,0 +1,52 @@
+#include <Keypad.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {9, 8, 7, 6}; //connect to the column pinouts of the keypad
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+//Create an object of keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup(){
+  Serial.begin(9600);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+}
+  
+void loop(){
+  char key = keypad.getKey();// Read the key
+  
+  // Print if key pressed
+  if (key){
+    Serial.print("Key Pressed : ");
+    Serial.println(key);
+    display.println(key);
+    display.display();
+    display.clearDisplay();
+    display.setCursor(0, 0);
+  }
+}
