6d16d886e25ea2cbc63eb8a043bd086a1dba6b64
Naor Livne <naorlivne@gmail.com>
2021-09-12 18:25:15 +0300

    starting work on arming procedure

--- software/arduino_tnt_defuse_game/arduino_tnt_defuse_game.ino
+++ software/arduino_tnt_defuse_game/arduino_tnt_defuse_game.ino
@@ -8,8 +8,8 @@
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
 
-const int buzzer_pin =  1;// the number of the buzzer pin
-const int cuttable_wires_out =  10;// pin of fake-cuttable wire output
+const int buzzer_pin =  10;// the number of the buzzer pin
+const int cuttable_wires_out =  1;// pin of fake-cuttable wire output
 const int cuttable_wires_in[]={11, 12, 13}; // pins of fake-cuttable wires input
 
 char keys[ROWS][COLS] = {
@@ -21,6 +21,15 @@ char keys[ROWS][COLS] = {
 
 byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {9, 8, 7, 6}; //connect to the column pinouts of the keypad
+
+// change to true if you want buzzing on key presses
+bool buzz_on_key_press = false;
+
+// set bomb starting state to armed, as switch doesn't allow string it will be mapped by the following table
+// 0 = arming
+// 1 = counting
+// 2 = exploded
+int bomb_state = 0;
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
@@ -45,28 +54,51 @@ void setup(){
   for (byte i = 0; i < 3; i = i + 1) {
     pinMode(cuttable_wires_in[i], INPUT);
   }
+
 }
 
+
+// ring buzzer
 void ring_buzzer_on(){
   digitalWrite(buzzer_pin, HIGH);
 }
 
-
+// stop ringing buzzer
 void ring_buzzer_off(){
   digitalWrite(buzzer_pin, LOW);
 }
   
 void loop(){
+  
 
-  char key = keypad.getKey();// Read the key
-  // Print if key pressed
-  if (key){
+  switch (bomb_state) {
+  case 0:
+     char key = keypad.getKey();// Read the key
+    // Print if key pressed
+    if (key){
+      if (buzz_on_key_press == true){
+        ring_buzzer_on();
+        delay(100);
+        ring_buzzer_off();
+      }
+      display.println("enter defuse code:");
+      display.display();
+      display.setCursor(0, 16);
+      display.println(key);
+      display.display();
+      display.clearDisplay();
+      display.setCursor(0, 0);
+  }
+    break;
+  case 1:
+    //do something when var equals 2
+    break;
+  case 2:
     ring_buzzer_on();
-    delay(1000);
-    ring_buzzer_off();
-    display.println(key);
+    display.println("bomb exploded");
     display.display();
     display.clearDisplay();
     display.setCursor(0, 0);
+    break;
   }
 }
